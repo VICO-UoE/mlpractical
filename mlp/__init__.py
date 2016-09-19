e9502c44a4d4a0ab17c743e3ac5b0126b3cabbbe
Matt Graham <m.m.graham@ed.ac.uk>
2016-09-19 11:14:10 +0100

    Moving default seed to package level and adding author metadata.

---
+++ mlp/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Machine Learning Practical package."""
+
+__authors__ = ['Pawel Swietojanski', 'Steve Renals', 'Matt Graham']
+
+DEFAULT_SEED = 123456  # Default random number generator seed if none provided.
