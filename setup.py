8563a47b65bb0b12714a95e34c391b2e45ae6850
Matt Graham <m.m.graham@ed.ac.uk>
2016-09-21 00:53:52 +0100

    Adding additional authors to metadata.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "mlp",
-    author = "Pawel Swietojanski",
+    author = "Pawel Swietojanski, Steve Renals and Matt Graham",
     description = ("Neural network framework for University of Edinburgh "
                    "School of Informatics Machine Learning Practical course."),
     url = "https://github.com/CSTR-Edinburgh/mlpractical",
