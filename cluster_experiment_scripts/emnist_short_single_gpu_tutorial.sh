c9f200662966a6258f9368debfc99ef17c66a6d7
AntreasAntoniou <antrikohs@gmail.com>
2020-01-12 12:25:38 +0000

    Update mlp cluster tutorial

--- cluster_experiment_scripts/emnist_short_single_gpu_tutorial.sh
+++ cluster_experiment_scripts/emnist_short_single_gpu_tutorial.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Short
+#SBATCH --partition=Teach-Short
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-03:59:00
--- cluster_experiment_scripts/emnist_standard_multi_gpu_tutorial.sh
+++ cluster_experiment_scripts/emnist_standard_multi_gpu_tutorial.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Standard
+#SBATCH --partition=Teach-Standard*
 #SBATCH --gres=gpu:4
 #SBATCH --mem=12000  # memory in Mb
 #SBATCH --time=0-08:00:00
