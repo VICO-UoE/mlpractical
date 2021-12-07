20755ffce210d02ac134b165f776f844eadee5bf
tianBreznik <tian.breza@gmail.com>
2021-12-07 22:07:11 +0000

    yet another one

--- pytorch_mlp_framework/model_architectures.py
+++ pytorch_mlp_framework/model_architectures.py
@@ -452,6 +452,6 @@ class ConvolutionalBatchNormDimensionalityReductionBlock(nn.Module):
         out = self.layer_dict['conv_1'].forward(out)
         #print("conv_1 fprop output:" + out.shape)
         out = F.leaky_relu(out)
-        out = self.layer_dict['bn_1'][out]
+        out = self.layer_dict['bn_1'](out)
 
         return out
