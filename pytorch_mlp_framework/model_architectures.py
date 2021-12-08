6b8289ac3cc66aa5098b9aff93a34c357c6f661f
tianBreznik <tian.breza@gmail.com>
2021-12-08 21:13:27 +0000

    more printing

--- pytorch_mlp_framework/model_architectures.py
+++ pytorch_mlp_framework/model_architectures.py
@@ -531,7 +531,7 @@ class ConvolutionalBatchNormRCDimensionalityReductionBlock(nn.Module):
         x = torch.zeros(self.input_shape)
         out = x
 
-        self.layer_dict['conv_0'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters * 2, bias=self.bias,
+        self.layer_dict['conv_0'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters, bias=self.bias,
                                               kernel_size=self.kernel_size, dilation=self.dilation,
                                               padding=self.padding, stride=1)
 
@@ -541,11 +541,13 @@ class ConvolutionalBatchNormRCDimensionalityReductionBlock(nn.Module):
         self.layer_dict['bn_0'] = nn.BatchNorm2d(out.shape[1],affine=True)
         out = self.layer_dict['bn_0'](out)
         
+        print("before pool")
+        print(out.shape)
         out = F.avg_pool2d(out, self.reduction_factor)
-
+        print(out.shape)
         self.layer_dict['conv_1'] = nn.Conv2d(in_channels=out.shape[1], out_channels=self.num_filters, bias=self.bias,
                                               kernel_size=self.kernel_size, dilation=self.dilation,
-                                              padding=self.padding, stride=2)
+                                              padding=self.padding, stride=1)
 
         out = self.layer_dict['conv_1'].forward(out)
         out = F.leaky_relu(out)
